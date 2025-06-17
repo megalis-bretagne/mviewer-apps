bbd272a85435b99463b50a21f1c95914869a60dc
mad <accompagnement@megalis.bretagne.bzh>
2025-06-17 15:44:28 +0000

    Correction affichage couche intercommunalités

--- megalis-services/customlayers/epci.js
+++ megalis-services/customlayers/epci.js
@@ -2,7 +2,7 @@
   mviewer.customLayers.epci = {};
   mviewer.customLayers.epci.layer = new ol.layer.Vector({
           source: new ol.source.Vector({
-              url: "https://geobretagne.fr/geoserver/megalis/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GETFEATURE&TYPENAME=services_organisme&CQL_FILTER=annee=%272022%27%20AND%20level=%27Intercommunalit%C3%A9%27&outputFormat=application/json&srsName=EPSG:4326",
+              url: "https://geobretagne.fr/geoserver/megalis/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GETFEATURE&TYPENAME=services_organisme&CQL_FILTER=annee=%272025%27%20AND%20level=%27Intercommunalit%C3%A9%27&outputFormat=application/json&srsName=EPSG:4326",
               format: new ol.format.GeoJSON()
           }),
   style: new ol.style.Style({
